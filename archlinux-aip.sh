d1ed37fb3da0cd98c00dc162c9a1fd97025d66d2
Liam Powell <60463234+gfelipe099@users.noreply.github.com>
2020-08-31 10:35:01 +0000

    ClamAV configuration finished

--- archlinux-aip.sh
+++ archlinux-aip.sh
@@ -278,6 +278,31 @@ function extrasSetup() {
     fi
     yay -Syu --noconfirm && yay -S ${packagesArch} ${packagesAur} ${packagesAurEol} --noconfirm --needed
     freshclam && systemctl enable --now clamav-freshclam
+    printf "#!/bin/bash
+PATH=/usr/bin
+alert="Signature detected: $CLAM_VIRUSEVENT_VIRUSNAME in $CLAM_VIRUSEVENT_FILENAME"
+
+# Send the alert to systemd logger if exist, othewise to /var/log
+if [[ -z $(command -v systemd-cat) ]]; then
+        echo "$(date) - $alert" >> /var/log/clamav/detections.log
+else
+        # This could cause your DE to show a visual alert. Happens in Plasma, but the next visual alert is much nicer.
+        echo "$alert" | /usr/bin/systemd-cat -t clamav -p emerg
+fi
+
+# Send an alert to all graphical users.
+XUSERS=($(who|awk '{print $1$NF}'|sort -u))
+
+for XUSER in $XUSERS; do
+    NAME=(${XUSER/(/ })
+    DISPLAY=${NAME[1]/)/}
+    DBUS_ADDRESS=unix:path=/run/user/$(id -u ${NAME[0]})/bus
+    echo "run $NAME - $DISPLAY - $DBUS_ADDRESS -" >> /tmp/testlog 
+    /usr/bin/sudo -u ${NAME[0]} DISPLAY=${DISPLAY} \
+                       DBUS_SESSION_BUS_ADDRESS=${DBUS_ADDRESS} \
+                       PATH=${PATH} \
+                       /usr/bin/notify-send -i dialog-warning "clamAV" "$alert"
+done" > /etc/clamav/detected.sh && aa-complain clamd &>/dev/null && sed -i 's/#User clamav/User root/g' /etc/clamav/clamd.conf && sed -i 's/#LocalSocket /run/clamav/clamd.ctl/LocalSocket /run/clamav/clamd.ctl/g' /etc/clamav/clamd.conf && sudo systemctl restart clamav-daemon
     xdg-user-dirs-update
 }
 
